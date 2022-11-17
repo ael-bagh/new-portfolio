3882378848af54918a329385db413e97e932e31d
Anas El Baghdadi <ael-bagh@student.1337.ma>
2022-11-18 00:17:22 +0100

    fixed the id for navigation

--- pages/components/Skills.js
+++ pages/components/Skills.js
@@ -1,7 +1,7 @@
 import Stack from "./Stack"
 function Skills() {
   return (
-    <div className="flex flex-col h-screen bg-gray-900 p-10 justify-center gap-3" id="education">
+    <div className="flex flex-col h-screen bg-gray-900 p-10 justify-center gap-3" id="skills">
     <div className='text-3xl font-bold mb-3'>Skills</div>
     <Stack/>
 </div>
